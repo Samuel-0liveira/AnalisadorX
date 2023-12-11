2c413ead335b5d0e6ca37cf9c81d8c4ca03d854d
Samuel Oliveira <S.SamuelS.a.m@hotmail.com>
2023-12-11 13:40:57 -0300

    Criação de tela para configuração do banco de dados.

---
+++ Banco de Dados/ConfiguracaoBanco.Designer.cs
@@ -0,0 +1,207 @@
+﻿namespace AnalisadorX
+{
+    partial class Frm_ConfiguracaoBanco
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.txt_InformacaoPorta = new System.Windows.Forms.TextBox();
+            this.txt_InformacaoUser = new System.Windows.Forms.TextBox();
+            this.txt_InformacaoSenha = new System.Windows.Forms.TextBox();
+            this.txt_InformacaoData = new System.Windows.Forms.TextBox();
+            this.btn_SalvarInfo = new System.Windows.Forms.Button();
+            this.txt_InformacaoServidor = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(92, 70);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(378, 18);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Preencha os campos com as informações requisitadas.";
+            // 
+            // label2
+            // 
+            this.label2.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Franklin Gothic Medium", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(92, 134);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(66, 24);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Server:";
+            // 
+            // label3
+            // 
+            this.label3.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Franklin Gothic Medium", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(92, 180);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(48, 24);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Port:";
+            // 
+            // label4
+            // 
+            this.label4.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Franklin Gothic Medium", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(92, 222);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(73, 24);
+            this.label4.TabIndex = 3;
+            this.label4.Text = "User ID:";
+            // 
+            // label5
+            // 
+            this.label5.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Franklin Gothic Medium", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.Location = new System.Drawing.Point(92, 265);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(93, 24);
+            this.label5.TabIndex = 4;
+            this.label5.Text = "Password:";
+            // 
+            // label6
+            // 
+            this.label6.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Franklin Gothic Medium", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label6.Location = new System.Drawing.Point(92, 309);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(92, 24);
+            this.label6.TabIndex = 5;
+            this.label6.Text = "Database:";
+            // 
+            // txt_InformacaoPorta
+            // 
+            this.txt_InformacaoPorta.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.txt_InformacaoPorta.Location = new System.Drawing.Point(282, 177);
+            this.txt_InformacaoPorta.Name = "txt_InformacaoPorta";
+            this.txt_InformacaoPorta.Size = new System.Drawing.Size(188, 21);
+            this.txt_InformacaoPorta.TabIndex = 7;
+            // 
+            // txt_InformacaoUser
+            // 
+            this.txt_InformacaoUser.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.txt_InformacaoUser.Location = new System.Drawing.Point(282, 219);
+            this.txt_InformacaoUser.Name = "txt_InformacaoUser";
+            this.txt_InformacaoUser.Size = new System.Drawing.Size(188, 21);
+            this.txt_InformacaoUser.TabIndex = 8;
+            // 
+            // txt_InformacaoSenha
+            // 
+            this.txt_InformacaoSenha.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.txt_InformacaoSenha.Location = new System.Drawing.Point(282, 262);
+            this.txt_InformacaoSenha.Name = "txt_InformacaoSenha";
+            this.txt_InformacaoSenha.Size = new System.Drawing.Size(188, 21);
+            this.txt_InformacaoSenha.TabIndex = 9;
+            // 
+            // txt_InformacaoData
+            // 
+            this.txt_InformacaoData.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.txt_InformacaoData.Location = new System.Drawing.Point(282, 306);
+            this.txt_InformacaoData.Name = "txt_InformacaoData";
+            this.txt_InformacaoData.Size = new System.Drawing.Size(188, 21);
+            this.txt_InformacaoData.TabIndex = 10;
+            // 
+            // btn_SalvarInfo
+            // 
+            this.btn_SalvarInfo.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.btn_SalvarInfo.Font = new System.Drawing.Font("Franklin Gothic Medium", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn_SalvarInfo.Location = new System.Drawing.Point(187, 381);
+            this.btn_SalvarInfo.Name = "btn_SalvarInfo";
+            this.btn_SalvarInfo.Size = new System.Drawing.Size(187, 32);
+            this.btn_SalvarInfo.TabIndex = 11;
+            this.btn_SalvarInfo.Text = "Salvar configurações";
+            this.btn_SalvarInfo.UseVisualStyleBackColor = true;
+            // 
+            // txt_InformacaoServidor
+            // 
+            this.txt_InformacaoServidor.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.txt_InformacaoServidor.Location = new System.Drawing.Point(282, 134);
+            this.txt_InformacaoServidor.Name = "txt_InformacaoServidor";
+            this.txt_InformacaoServidor.Size = new System.Drawing.Size(188, 21);
+            this.txt_InformacaoServidor.TabIndex = 6;
+            // 
+            // Frm_ConfiguracaoBanco
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None;
+            this.ClientSize = new System.Drawing.Size(560, 481);
+            this.ControlBox = false;
+            this.Controls.Add(this.btn_SalvarInfo);
+            this.Controls.Add(this.txt_InformacaoData);
+            this.Controls.Add(this.txt_InformacaoSenha);
+            this.Controls.Add(this.txt_InformacaoUser);
+            this.Controls.Add(this.txt_InformacaoPorta);
+            this.Controls.Add(this.txt_InformacaoServidor);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Font = new System.Drawing.Font("Franklin Gothic Medium", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
+            this.MaximizeBox = false;
+            this.Name = "Frm_ConfiguracaoBanco";
+            this.ShowIcon = false;
+            this.WindowState = System.Windows.Forms.FormWindowState.Maximized;
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.TextBox txt_InformacaoPorta;
+        private System.Windows.Forms.TextBox txt_InformacaoUser;
+        private System.Windows.Forms.TextBox txt_InformacaoSenha;
+        private System.Windows.Forms.TextBox txt_InformacaoData;
+        private System.Windows.Forms.Button btn_SalvarInfo;
+        private System.Windows.Forms.TextBox txt_InformacaoServidor;
+    }
+}
