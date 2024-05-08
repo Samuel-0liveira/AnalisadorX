1b2f598ed4eabdc62e3e6df8aa08b531d7cde437
Samuel Oliveira <S.SamuelS.a.m@hotmail.com>
2024-05-07 22:50:57 -0300

    Comentários nas linhas 9, 18 e 24 a 26 em CatalogoDeMutantes.cs

--- CatalogoDeMutantes.cs
+++ CatalogoDeMutantes.cs
@@ -6,6 +6,7 @@ namespace AnalisadorX
 {
     public partial class Frm_CatalogoDeMutantes : Form
     {
+        //Chama método para conectar ao banco de dados.
         Conexao conn = new Conexao();
         public Frm_CatalogoDeMutantes()
         {
@@ -14,11 +15,16 @@ namespace AnalisadorX
 
         private void Frm_CatalogoDeMutantes_Load(object sender, EventArgs e)
         {
+            //Chama método do banco para incluir informações no banco de dados.
             conn.IncluirCatalogo(cb_Mutantes);
         }
 
         private void cb_Mutantes_SelectedIndexChanged(object sender, EventArgs e)
         {
+            /*Caso a combobox tenha algum valor selecionado, altera as labels de acordo
+             *com as informações do banco de dados baseada no campo alterego.
+             */
+            
             if (cb_Mutantes != null)
             {
                 conn.AlterarInfosLabel(cb_Mutantes.Text, lbl_N, lbl_A, lbl_I, lbl_F, lbl_H, lbl_C);
