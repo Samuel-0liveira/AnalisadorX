c6b03508b00a44b5f2ca7a0fb9adeade3d50d0f6
Samuel Oliveira <S.SamuelS.a.m@hotmail.com>
2024-05-08 23:53:49 -0300

    Comentários linhas: 27 e 30 em TelaDeLogin.cs

--- TelaDeLogin.cs
+++ TelaDeLogin.cs
@@ -24,8 +24,10 @@ namespace AnalisadorX
             string usuario = txt_User.Text;
             string senha = txt_Senha.Text;
 
+            //Chama o método RealizarLogin para ver se as informações digitadas batem com a do banco de dados.
             bool loginEfetuado = con.RealizarLogin(usuario, senha);
 
+            //Exibi uma mensagem diferente baseado no retorno da variável loginEfetuado.
             if (loginEfetuado)
             {
                 MessageBox.Show("Login realizado com sucesso!");
